29bac48d184e009e3e4aaf2dc81c2782bd6e8788
Aasif Ahmed <asifarslaan@gmail.com>
2024-01-15 18:35:33 +0530

    ffsf

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -804,7 +804,7 @@
         })
 
     </script>
-
+<script src="{{asset('js/bundle.js')}}" defer=""></script>
     <script src="{{asset('js/vendors~chunk-pica.48c160a010746ad5f05b.js')}}" defer=""></script>
     <script src="{{asset('js/chunck-signup-form.96dda844e6d6b7cae628.js')}}" defer=""></script>
 </body>
