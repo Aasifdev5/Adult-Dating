09c5ddfb33d432983f32d2d02744ddd74d8f0ab0
Aasif Ahmed <asifarslaan@gmail.com>
2024-01-16 16:31:53 +0530

    f

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -114,7 +114,7 @@ Home
                     <div class="col-sm-4 text-center">
                         <img src="{{ asset('e29d6fb82c4a45e1b0eeb41d7b11577f.jpg') }}" style="height: 200px; width: 250px;" alt="Profile Image" class="profile-image rounded-circle">
                         <p class="mb-1 text-center">{{ $ad->title }}</p>
-                        <h3 class="mb-3 text-center">Ada</h3>
+                        <h3 class="mb-3 text-center">{{$ad->name}}</h3>
                         <h3 class="mb-3 text-center">{{$ad->age}} Years</h3>
                         <h5 class="text-muted text-center"><i class="icon icon-map-pin mr-1"></i>{{ $ad->city }}</h5>
                     </div>
