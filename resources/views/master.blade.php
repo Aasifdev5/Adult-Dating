a6943224abf36aa82a427a99ef60a18690339624
Aasif Ahmed <asifarslaan@gmail.com>
2024-01-16 13:05:24 +0530

    h

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -539,7 +539,7 @@
                             @else
 
                             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                                <a class="navbar-brand" href="/">Your Logo</a>
+
                                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                                     <span class="navbar-toggler-icon"></span>
                                 </button>
